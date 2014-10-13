14a2421db9a6078fcf0b4f226496c1fb3a25d1e8
Min Huang <min.huang@alumni.usc.edu>
2014-10-13 02:33:51 -0700

    Fix issue with parsing dirty

--- base/.bash_libs/repo.sh
+++ base/.bash_libs/repo.sh
@@ -24,8 +24,8 @@ function repo::branch {
 # Returns an asterisk if the branch you are on is dirty (has changes).
 function repo::dirty {
   M1="nothing to commit, working directory clean"
-  M1="nothing to commit (working directory clean)"
+  M2="nothing to commit (working directory clean)"
   STATUS=$(git status 2> /dev/null | tail -n1)
-  [[ $STATUS != $M1 && $STATUS != $M2 ]] && echo "*"
+  [[ $STATUS != $M1 && $STATUS != $M2 ]] && echo '*'
 }
 
