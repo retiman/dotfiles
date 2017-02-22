935f9c75b8a9ca2917afabd53da2614db006f988
Min Huang <mihuang@linkedin.com>
2017-02-21 18:01:54 -0800

    Now able to deal with svn repos

--- base/.bash_libs/repo.sh
+++ base/.bash_libs/repo.sh
@@ -17,16 +17,35 @@ function repo::root() {
 
 # Returns the branch you are on, with an asterisk if the branch has changes.
 function repo::branch() {
-  git branch --no-color 2> /dev/null |
-  sed -e '/^[^*]/d' -e "s/* \(.*\)/\(\1$(repo::dirty)\)/"
+  local URL=$(svn info 2> /dev/null | awk '/URL:/ {print $2}')
+  case "$URL" in
+    */trunk)
+      echo "(trunk$(repo::svndirty))"
+      ;;
+    */branches)
+      echo "(branches$(repo::svndirty))"
+      ;;
+    */tags)
+      echo "(tags$(repo::svndirty))"
+      ;;
+    *)
+      git branch --no-color 2> /dev/null |
+      sed -e '/^[^*]/d' -e "s/* \(.*\)/\(\1$(repo::gitdirty)\)/"
+      ;;
+  esac
 }
 
 # Returns an asterisk if the branch you are on is dirty (has changes).
-function repo::dirty() {
-  M1="nothing to commit, working tree clean"
-  M2="nothing to commit (working directory clean)"
-  M3="nothing to commit, working directory clean"
-  STATUS=$(git status 2> /dev/null | tail -n1)
+function repo::gitdirty() {
+  local M1="nothing to commit, working tree clean"
+  local M2="nothing to commit (working directory clean)"
+  local M3="nothing to commit, working directory clean"
+  local STATUS=$(git status 2> /dev/null | tail -n1)
   [[ $STATUS != $M1 && $STATUS != $M2 && $STATUS != $M3 ]] && echo '*'
 }
 
+# Returns an asterisk if the branch you are on is dirty (has changes).
+function repo::svndirty() {
+  local STATUS=$(svn status | awk '/^(A|M|?)/')
+  [[ ! -z $STATUS ]] && echo '*'
+}
