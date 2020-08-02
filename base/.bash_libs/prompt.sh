49361037359a99047e7a986bf4a0ac8d222b70aa
Min Huang <min.huang@alumni.usc.edu>
2020-08-01 19:43:44 -0700

    Fixes colors in Bash

--- base/.bash_libs/prompt.sh
+++ base/.bash_libs/prompt.sh
@@ -1,7 +1,7 @@
 # Colorize the Bash prompt the way I like it
 function prompt::colorize() {
   local COLOR=$1
-  PS1="\[\033[G\]$COLOR[\u@\h:\W$COLOR_RED\$(repo::branch)$COLOR]\$\[\033[0m\] "
+  PS1="$COLOR[\u@\h:\W$COLOR_RED\$(repo::branch)$COLOR]\$$COLOR_NORMAL "
   PS2='> '
   PS4='+ '
 }
