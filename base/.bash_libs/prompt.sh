7026e7b113494d7e47e3d050d5b32a7b64c25848
Min Huang <min.huang@alumni.usc.edu>
2021-03-24 22:30:39 -0700

    Tries to improve performance with git

--- base/.bash_libs/prompt.sh
+++ base/.bash_libs/prompt.sh
@@ -1,8 +1,16 @@
 # Colorize the Bash prompt the way I like it
-function prompt::colorize() {
+function prompt::colorizewithgit() {
   local COLOR=$1
   PS1="$COLOR[\u@\h:\W$COLOR_RED\$(repo::branch)$COLOR]\$$COLOR_NORMAL "
   PS2='> '
   PS4='+ '
 }
 
+# WSL2 performance across filesystems is quite poor, so running git commands
+# every time you get a prompt is slow as molasses.
+function prompt::colorize() {
+  local COLOR=$1
+  PS1="$COLOR[\u@\h:\W]\$$COLOR_NORMAL "
+  PS2='> '
+  PS4='+ '
+}
