f4d118752b6123c414c66d31e528285529695683
Min Huang <min.huang@alumni.usc.edu>
2021-04-27 03:58:42 -0700

    Using posh-git with PowerShell now

--- base/.bash_libs/prompt.sh
+++ base/.bash_libs/prompt.sh
@@ -1,16 +1,6 @@
-# Colorize the Bash prompt the way I like it
-function prompt::colorizewithgit() {
+function prompt::setup() {
   local COLOR=$1
   PS1="$COLOR[\u@\h:\W$COLOR_RED\$(repo::branch)$COLOR]\$$COLOR_NORMAL "
-  PS2='> '
-  PS4='+ '
-}
-
-# WSL2 performance across filesystems is quite poor, so running git commands
-# every time you get a prompt is slow as molasses.
-function prompt::colorize() {
-  local COLOR=$1
-  PS1="$COLOR[\u@\h:\W]\$$COLOR_NORMAL "
   PS2='> '
   PS4='+ '
 }
