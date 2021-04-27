1c6817e9fb833c5a571c73b9d91be6bfefdbc0ad
Min Huang <min.huang@alumni.usc.edu>
2021-04-27 04:25:21 -0700

    Just use vim for this

--- base/.bash_libs/common.sh
+++ base/.bash_libs/common.sh
@@ -1,11 +1,3 @@
 function addpath() {
   PATH="$1:$PATH"
 }
-
-function decrypt() {
-  openssl enc -d -aes-256-cbc -salt -in "$1" -out "${1/.aes/}"
-}
-
-function encrypt() {
-  openssl enc -e -aes-256-cbc -salt -in "$1" -out "$1.aes"
-}
